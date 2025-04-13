31d548e73cf87031400c1e196bda396f9b14863d
Abu Said Mohammad Readwanul <abusaidhridoy5@gmail.com>
2025-04-14 00:29:53 +0600

    Third

--- Edulearn/settings.py
+++ Edulearn/settings.py
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
 ]
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
